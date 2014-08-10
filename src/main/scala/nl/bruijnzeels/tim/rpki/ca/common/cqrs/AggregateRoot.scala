85b8e19db8f4ebea822b2bffdc199907f6d105d3
Tim Bruijnzeels <tim.bruijnzeels@gmail.com>
2014-08-10 14:27:39 +0200

    Adding reminder that, at some point, there should be versioning on aggregate to make it possible to detect concurrent updates, and handle possible conflicts

--- src/main/scala/nl/bruijnzeels/tim/rpki/ca/common/cqrs/AggregateRoot.scala
+++ src/main/scala/nl/bruijnzeels/tim/rpki/ca/common/cqrs/AggregateRoot.scala
@@ -1,6 +1,9 @@
 package nl.bruijnzeels.tim.rpki.ca.common.cqrs
 
+import java.util.UUID
+
 trait AggregateRoot {
+  def id: UUID // TODO: Have versions to detect concurrent updates
   def applyEvents(events: List[Event]): AggregateRoot
   def clearEventList(): AggregateRoot
 }
